4f3dbf337e4b25e322c7015787937faec3f7f3e9
Saharnaz1 <sa_rdh@yahoo.com>
2023-11-01 17:27:00 -0400

    create VehiclesPage_Sahar

--- src/test/java/com/danica_intabella/pages/VehiclesPage_Sahar.java
+++ src/test/java/com/danica_intabella/pages/VehiclesPage_Sahar.java
@@ -1,4 +1,50 @@
 package com.danica_intabella.pages;
 
+import com.danica_intabella.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class VehiclesPage_Sahar {
-}
+
+    public VehiclesPage_Sahar() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+
+    @FindBy(xpath = "//span[.='Vehicles']")
+    public WebElement fleetDropdown;
+
+    @FindBy(xpath = "//a[.='...']")
+    public WebElement threeDotsLocator;
+
+    @FindBy(xpath = "//a[@title='View']")
+    public WebElement hoverView;
+
+    @FindBy(xpath = "//a[@title='Edit']")
+    public WebElement hoverEdit;
+
+    @FindBy(xpath = "//a[@title='Delete']")
+    public WebElement hoverDelete;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
