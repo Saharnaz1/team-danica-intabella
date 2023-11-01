e37dd9b8253f5089d5e8b7be3f4856a9d9ac64c3
PuujeeS <puujeesodnom.mail@gmail.com>
2023-11-01 08:38:15 -0400

    Created US02 step defs

---
+++ src/test/java/com/danica_intabella/step_definitions/US02_StepDefs.java
@@ -0,0 +1,27 @@
+package com.danica_intabella.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US02_StepDefs {
+
+    @Given("the user login with {string},{string}")
+    public void the_user_login_with(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User should be able to click on the question icon")
+    public void user_should_be_able_to_click_on_the_question_icon() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User sees Oro Documentation page")
+    public void user_sees_oro_documentation_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
