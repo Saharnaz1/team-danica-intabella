6b0e0fdaceb7f81eef2a56a3d561cba3d981d5f4
florentbooumabayalaflorent <bbflorent0@gmail.com>
2023-11-06 12:03:03 -0500

    Later update

--- src/test/java/com/danica_intabella/pages/US01_Module_Page.java
+++ src/test/java/com/danica_intabella/pages/US01_Module_Page.java
@@ -17,7 +17,6 @@ public class US01_Module_Page {
 
     }
     @FindBy(xpath = "//span[@class='title title-level-1']")
-
     public List<WebElement> AllTopModules;
 
 
--- src/test/java/com/danica_intabella/step_definitions/US01_Modules_Step_def.java
+++ src/test/java/com/danica_intabella/step_definitions/US01_Modules_Step_def.java
@@ -2,6 +2,7 @@ package com.danica_intabella.step_definitions;
 
 
 import com.danica_intabella.pages.US01_Module_Page;
+import com.danica_intabella.utilities.BrowserUtils;
 import com.google.common.collect.Table;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
@@ -15,18 +16,21 @@ public class US01_Modules_Step_def {
     US01_Module_Page us01ModulePage=  new US01_Module_Page();
 
     @Then("user should view eight following modules")
-    public void user_should_view_eight_following_modules(List<String> ExpectedModules) {
+    public void user_should_view_eight_following_modules(List<String> expectedModules) {
 
 
-        List<String> actualModules=  new ArrayList<>();
+      //  List<String> actualModules=  new ArrayList<>();
 
-        for (WebElement eacTopModule : us01ModulePage.AllTopModules) {
+       // for (WebElement eacTopModule : us01ModulePage.AllTopModules) {
 
-            actualModules.add(eacTopModule.getText());
+       //     actualModules.add(eacTopModule.getText());
 
-        }
-
-        Assert.assertEquals(ExpectedModules,actualModules);
+       // }
+        BrowserUtils.sleep(3);
+        List<String> actualModules = BrowserUtils.getElementsText(us01ModulePage.AllTopModules);
+        System.out.println("actualModules = " + actualModules);
+        System.out.println("expectedModules = " + expectedModules);
+        Assert.assertEquals(expectedModules,actualModules);
 
 
 
